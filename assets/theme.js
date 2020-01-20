1264bb81b5e376b55f6f2de215ef156f56c697c6
EdsonSoni <edson.soni@terrabionic.com.mx>
2020-01-20 13:24:09 -0600

    agregado js y ajustes base para carousel

--- assets/theme.js
+++ assets/theme.js
@@ -5163,3 +5163,59 @@ theme.init = function() {
 };
 
 $(theme.init);
+
+
+/* Carousel product-recomendations */
+
+$(".js-carousel").each(function(){
+	var $carousel = $(this),
+		$carouselContainer = $carousel.find(".js-carousel-container"),
+		$carouselList = $carousel.find(".js-carousel-list"),
+		$carouselItem = $carousel.find(".js-carousel-item"),
+		$carouselButton = $carousel.find(".js-carousel-button"),
+		setItemWidth = function(){
+			$carouselList.removeAttr("style");
+			var curWidth = $($carouselItem[0]).outerWidth() * $carouselItem.length;
+			$carouselList.css("width", curWidth);
+		},
+		slide = function(){
+			var $button = $(this),
+				dir = $button.data("dir"),
+				curPos = parseInt($carouselList.css("left")) || 0,
+				moveto = 0,
+				containerWidth = $carouselContainer.outerWidth(),
+				listWidth = $carouselList.outerWidth(),
+				before = (curPos + containerWidth),
+        after = listWidth + (curPos - containerWidth);
+     
+			if(dir=="next"){
+				moveto = (after < containerWidth) ? curPos - after : curPos - containerWidth;
+			} else {
+				moveto = (before >= 0) ? 0 : curPos + containerWidth;
+			}
+      console.log('before='+ before);
+      console.log('curpos='+curPos);
+      console.log('moveTo='+ moveto);
+      console.log('listWidth='+ listWidth);
+      console.log('containerWidth='+ containerWidth);
+      console.log('___________________');
+
+  
+     
+      
+      moveto = ((dir == "next") && (curPos == moveto)) ? 0 : moveto;
+      
+     moveto = ((dir == "prev")&& (curPos == moveto)) ? -(listWidth - containerWidth) : moveto;
+     
+			
+			$carouselList.animate({
+				left: moveto
+			});
+		};
+	$(window).resize(function(){
+		setItemWidth();
+	});
+	setItemWidth();
+	
+	$carouselButton.on("click", slide);
+});
